060e10dec87d4f7ee547962ed867fa691131af0d
BRANDED-VPS <ggree297@gmail.com>
2025-01-27 14:29:57 +0530

    Add files via upload

---
+++ Nirajj.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7715266575:AAEIW9CEugqzshwSgCV2rJmYFZKjJEiEwGc")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@NIRAJModzOwner")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7097659861")
+
+
+
+
+
+
+
+  
