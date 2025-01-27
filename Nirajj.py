71aac46e1b87175016d7d2251d94e73fbd1863cb
BRANDED-VPS <ggree297@gmail.com>
2025-01-27 14:31:09 +0530

    Update Nirajj.py

--- Nirajj.py
+++ Nirajj.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7715266575:AAEIW9CEugqzshwSgCV2rJmYFZKjJEiEwGc")
+BOT_TOKEN=("7884177115:AAGslZvlIyCvD8mj5iq5CrNCiCFTme4plJk")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("@NIRAJModzOwner")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("7097659861")
+ADMIN_IDS=("1634510798")
 
 
 
